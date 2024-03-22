b5838498c51ecbdeddbef829f6d0e013fc5953d0
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-03-22 08:15:10 +0100

    postgres

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,6 +4,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailsLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
@@ -11,7 +12,8 @@ import { EmailService } from "./email/email.service";
 
 const logRepository = new LogRepositoryImpl(
     // new FileSystemDatasource()
-    new MongoLogDatasource(),
+    //new MongoLogDatasource(),
+    new PostgresLogDatasource(),
 );
 
 const emailService = new EmailService();
@@ -36,20 +38,20 @@ export class Server {
 
         // console.log(envs.MAILER_EMAIL, envs.MAILER_SECRET_KEY)
 
-        const logs = await logRepository.getLog(LogSeverityLevel.low);
-        console.log(logs)
+        // const logs = await logRepository.getLog(LogSeverityLevel.low);
+        // console.log(logs)
 
-        // CronService.createJob(
-        //     '*/5 * * * * *',
-        //     () => {
-        //         const url = 'https://google24dsfs.com';
-        //         new CheckService(
-        //             logRepository,
-        //             () => console.log(`${url} is ok`),
-        //             (error) => console.log(error)
-        //         ).execute(url)
-        //     }
-        // );
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                new CheckService(
+                    logRepository,
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
+                ).execute(url)
+            }
+        );
 
     }
 
